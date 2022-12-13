13e946eb33ad65cda377c20a49a689ed1014132d
Mike Robinson <mike.robinson@shopify.com>
2022-12-12 20:30:07 -0700

    day 11 complete

---
+++ day11/part2.js
@@ -0,0 +1,67 @@
+var lineReader = require('line-reader');
+
+const MAX_ROUNDS = 10000;
+//const RELAX_FACTOR = 3;
+
+class Monkey {
+  inspections = 0;
+  constructor(items, operation, test, destIfTrue, destIfFalse) {
+    this.items = items;
+    this.operation = operation;
+    this.test = test;
+    this.destIfTrue = destIfTrue;
+    this.destIfFalse = destIfFalse;
+  }
+  processRound() {
+    for (let i = 0; i < this.items.length; i++) {
+      this.inspections++;
+      this.items[i] = this.operation(this.items[i]);
+      this.items[i] = this.items[i] % FACTOR;
+      if (this.items[i] % this.test == 0) {
+        monkeys[this.destIfTrue].items.push(this.items[i]);
+      } else {
+        monkeys[this.destIfFalse].items.push(this.items[i]);
+      }
+    }
+    this.items = [];
+  }
+}
+
+let monkeys = [];
+
+// test
+// monkeys.push(new Monkey([79, 98], (old) => old * 19, 23, 2, 3));
+// monkeys.push(new Monkey([54, 65, 75, 74], (old) => old + 6, 19, 2, 0));
+// monkeys.push(new Monkey([79, 60, 97], (old) => old * old, 13, 1, 3));
+// monkeys.push(new Monkey([74], (old) => old + 3, 17, 0, 1));
+
+// actual
+monkeys.push(new Monkey([61], (old) => old * 11, 5, 7, 4));
+monkeys.push(new Monkey([76, 92, 53, 93, 79, 86, 81], (old) => old + 4, 2, 2, 6));
+monkeys.push(new Monkey([91, 99], (old) => old * 19, 13, 5, 0));
+monkeys.push(new Monkey([58, 67, 66], (old) => old * old, 7, 6, 1));
+monkeys.push(new Monkey([94, 54, 62, 73], (old) => old + 1, 19, 3, 7));
+monkeys.push(new Monkey([59, 95, 51, 58, 58], (old) => old + 3, 11, 0, 4));
+monkeys.push(new Monkey([87, 69, 92, 56, 91, 93, 88, 73], (old) => old + 8, 3, 5, 2));
+monkeys.push(new Monkey([71, 57, 86, 67, 96, 95], (old) => old + 7, 17, 3, 1));
+
+// had to look this up - multiplying the "test" values together gives
+// a number we can modulo the increasing large worry value by to keep it low
+const FACTOR = monkeys.reduce( (accumulator, monkey) => accumulator * monkey.test, 1);
+
+for (let round = 1; round <= MAX_ROUNDS; round++) {
+  monkeys.forEach( monkey => monkey.processRound());
+  if (round == 1 || round == 20 || round % 1000 == 0) {
+    console.log(`== After round ${round} ==`);
+    for (let i = 0; i<monkeys.length; i++) {
+      console.log(`Monkey ${i} inspected items ${monkeys[i].inspections} times.`);
+    }
+    console.log();
+  }
+}
+
+monkeys.sort((a,b) => b.inspections - a.inspections);
+console.log(monkeys[0].inspections * monkeys[1].inspections);
+
+
+
